4dd4d0b162b953d288f2bd3731545bc5014e1cb9
wakdeharshal-png <wakdeharshal@gmail.com>
2025-10-15 14:22:32 +0530

    Update main.py

--- git-repo/main.py
+++ git-repo/main.py
@@ -10,7 +10,7 @@ def main():
     print("5. Pull latest")
     print("6. Repo status")
     print("7. Create new GitHub repo")
-    print("7. Create new GitHub repo")
+    print("8. Create new GitHub repo")
     choice = input("Enter choice: ")
 
     if choice == "1":
